0e67189f8f460aaaa9956b7cbb6c34ac768a2acb
Chris Mytton <chrismytton@gmail.com>
2016-01-06 15:35:38 +0000

    Close duplicate Pull Requests on webhook
    When this application receives a webhook, check for other older pull
    requests that have the same title and leave a message on the referring
    to the newer Pull Request and then close them.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 
 ruby '2.3.0'
 
+gem 'sinatra'
+gem 'puma'
+gem 'sidekiq'
 gem 'octokit'
 gem 'pry'
 gem 'rake'
--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'bundler/setup'
+require 'json'
+require 'sinatra/base'
+require 'sidekiq'
 require 'octokit'
 require 'dotenv'
 Dotenv.load
 
 Octokit.auto_paginate = true
+
+GITHUB = Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
 
 class FindOutdatedPullRequests
   class Error < StandardError; end
@@ -23,17 +28,46 @@ class FindOutdatedPullRequests
     pull_requests.find_all do |p|
       p[:title] == pull[:title] &&
         p[:number] != pull[:number] &&
-        p[:created_at] <= pull[:created_at]
+        p[:created_at] <= pull[:created_at] &&
+        p[:user][:login] == pull[:user][:login]
     end
   end
 
   private
 
   def pull_requests
-    @pull_requests ||= github.pull_requests(repository)
+    @pull_requests ||= GITHUB.pull_requests(repository)
+  end
+end
+
+class PullRequestCleanerJob
+  include Sidekiq::Worker
+
+  def perform(pull_request_number)
+    finder = FindOutdatedPullRequests.new(everypolitician_data_repo)
+    outdated = finder.outdated_pull_requests(pull_request_number)
+    message = "This Pull Request has been superseded by ##{pull_request_number}"
+    outdated.each do |pull|
+      GITHUB.add_comment(everypolitician_data_repo, pull_request_number, message)
+      GITHUB.close_pull_request(everypolitician_data_repo, pull_request_number)
+    end
   end
 
-  def github
-    @github ||= Octokit::Client.new(access_token: ENV['GITHUB_ACCESS_TOKEN'])
+  def everypolitician_data_repo
+    ENV.fetch('EVERYPOLITICIAN_DATA_REPO', 'everypolitician/everypolitician-data')
+  end
+end
+
+class PullRequestCleaner < Sinatra::Base
+  get '/' do
+    'Send a POST request to this url to trigger the webhook'
+  end
+
+  post '/' do
+    return "Unhandled event" unless request.env['HTTP_X_EVERYPOLITICIAN_EVENT'] == 'pull_request_opened'
+    request.body.rewind
+    payload = JSON.parse(request.body.read)
+    pull_request_number = payload['pull_request_url'].split('/').last
+    PullRequestCleanerJob.perform_async(pull_request_number)
   end
 end
